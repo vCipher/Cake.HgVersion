815480bfe6782533fb55e2cd4b29a9e913de411e
Утин Артем Сергеевич <Utin.AS@partner.ru>
2017-12-29 13:11:49 +1000

    properly changed references to Hg.Net

--- src/HgVersionTests/TestVersionContext.cs
+++ src/HgVersionTests/TestVersionContext.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Threading;
 using HgVersion;
 using HgVersion.VCS;
-using Mercurial;
 using VCSVersion;
+using Mercurial;
 using VCSVersion.Configuration;
 using VCSVersion.Helpers;
 using VCSVersion.SemanticVersions;
@@ -21,7 +21,7 @@ namespace HgVersionTests
         static TestVesionContext()
         {
             // added for solve concurrency problems
-            if (!Client.CouldLocateClient)
+            if (!Mercurial.ClientExecutable.CouldLocateClient)
                 throw new MercurialMissingException("The Mercurial command line client could not be located");
         }
 
